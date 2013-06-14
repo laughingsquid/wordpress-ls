1228f4d8ca5744a2f38c4fc2fcb2cfc29e708cd2
Shelby DeNike <shelby@sd3labs.com>
2013-06-13 19:01:37 -0600

    Nightly Update

--- wp-content/plugins/jetpack/modules/contact-form/grunion-contact-form.php
+++ wp-content/plugins/jetpack/modules/contact-form/grunion-contact-form.php
@@ -13,8 +13,11 @@ License: GPLv2 or later
 define( 'GRUNION_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GRUNION_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-if ( is_admin() )
+if ( is_admin() ) {
 	require_once GRUNION_PLUGIN_DIR . '/admin.php';
+	if ( file_exists( GRUNION_PLUGIN_DIR . '/grunion-omnisearch.php' ) )
+		require_once GRUNION_PLUGIN_DIR . '/grunion-omnisearch.php';
+}
 
 /**
  * Sets up various actions, filters, post types, post statuses, shortcodes.
@@ -181,7 +184,7 @@ class Grunion_Contact_Form_Plugin {
 		if ( ! $submission_result ) {
 			header( "HTTP/1.1 500 Server Error", 500, true );
 			echo '<div class="form-error"><ul class="form-errors"><li class="form-error-message">';
-			esc_html_e( 'An error occurred. Please try again later.' , 'jetpack');
+			esc_html_e( 'An error occurred. Please try again later.', 'jetpack' );
 			echo '</li></ul></div>';
 		} elseif ( is_wp_error( $submission_result ) ) {
 			header( "HTTP/1.1 400 Bad Request", 403, true );
@@ -189,7 +192,7 @@ class Grunion_Contact_Form_Plugin {
 			echo esc_html( $submission_result->get_error_message() );
 			echo '</li></ul></div>';
 		} else {
-			echo '<h3>' . esc_html__( 'Message Sent' , 'jetpack') . '</h3>' . $submission_result;
+			echo '<h3>' . esc_html__( 'Message Sent', 'jetpack' ) . '</h3>' . $submission_result;
 		}
 
 		die;
@@ -998,7 +1001,9 @@ class Grunion_Contact_Form extends Crunion_Contact_Form_Shortcode {
 
 		$subject = apply_filters( 'contact_form_subject', $contact_form_subject );
 
-		$time = date_i18n( __( 'l F j, Y \a\t g:i a', 'jetpack' ), current_time( 'timestamp' ) );
+		$date_time_format = _x( '%1$s \a\t %2$s', '{$date_format} \a\t {$time_format}', 'jetpack' );
+		$date_time_format = sprintf( $date_time_format, get_option( 'date_format' ), get_option( 'time_format' ) );
+		$time = date_i18n( $date_time_format, current_time( 'timestamp' ) );
 
 		$extra_content = '';
 
@@ -1420,7 +1425,7 @@ function grunion_delete_old_spam() {
 	# nothing special about 5000 or 11
 	# just trying to periodically recover deleted rows
 	$random_num = mt_rand( 1, 5000 );
-	if ( apply_filters( 'grunion_optimize_table', ( $random_number == 11 ) ) ) {
+	if ( apply_filters( 'grunion_optimize_table', ( $random_num == 11 ) ) ) {
 		$wpdb->query( "OPTIMIZE TABLE $wpdb->posts" );
 	}
 
