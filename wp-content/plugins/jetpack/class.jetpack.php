56c35e8c09fdd25a30bdb4060beda7644f0a396b
Shelby DeNike <shelby@sd3labs.com>
2013-09-30 10:34:53 -0600

    Nightly Update

--- wp-content/plugins/jetpack/class.jetpack.php
+++ wp-content/plugins/jetpack/class.jetpack.php
@@ -48,6 +48,49 @@ class Jetpack {
 	);
 
 	/**
+	 * Map of modules that have conflicts with plugins and should not be auto-activated
+	 * if the plugins are active.  Used by filter_default_modules
+	 *
+	 * Plugin Authors: If you'd like to prevent a single module from auto-activating,
+	 * change `module-slug` and add this to your plugin:
+	 *
+	 * add_filter( 'jetpack_get_default_modules', 'my_jetpack_get_default_modules' );
+	 * function my_jetpack_get_default_modules( $modules ) {
+	 *     return array_diff( $modules, array( 'module-slug' ) );
+	 * }
+	 *
+	 * @var array
+	 */
+	private $conflicting_plugins = array(
+		'comments'          => array(
+			'Intense Debate'      => 'intensedebate/intensedebate.php',
+			'Disqus'              => 'disqus-comment-system/disqus.php',
+		),
+		'contact-form'      => array(
+			'Contact Form 7'      => 'contact-form-7/wp-contact-form-7.php',
+			'Gravity Forms'       => 'gravityforms/gravityforms.php',
+			'Contact Form Plugin' => 'contact-form-plugin/contact_form.php',
+			'Easy Contact Forms'  => 'easy-contact-forms/easy-contact-forms.php',
+		),
+		'gplus-authorship'  => array(
+			'WP SEO by Yoast'     => 'wordpress-seo/wp-seo.php',
+		),
+		'minileven'         => array(
+			'WPtouch'             => 'wptouch/wptouch.php',
+		),
+		'sharedaddy'        => array(
+			'AddThis'             => 'addthis/addthis_social_widget.php',
+			'Add To Any'          => 'add-to-any/add-to-any.php',
+			'ShareThis'           => 'share-this/sharethis.php',
+			'Shareaholic'         => 'shareaholic/shareaholic.php',
+		),
+		'widget-visibility' => array(
+			'Widget Logic'        => 'widget-logic/widget_logic.php',
+			'Dynamic Widgets'     => 'dynamic-widgets/dynamic-widgets.php',
+		),
+	);
+
+	/**
 	 * Message to display in admin_notice
 	 * @var string
 	 */
@@ -240,6 +283,8 @@ class Jetpack {
 		add_action( 'plugins_loaded', array( $this, 'check_rest_api_compat' ), 1000 );
 
 		add_filter( 'map_meta_cap', array( $this, 'jetpack_custom_caps' ), 1, 4 );
+
+		add_filter( 'jetpack_get_default_modules', array( $this, 'filter_default_modules' ) );
 	}
 
 	/**
@@ -314,14 +359,14 @@ class Jetpack {
 	}
 
 	/*
-	 * Returns the location of Jetpack's lib directory. This filter is applied  
-	 * in require_lib(). 
-	 * 
-	 * @filter require_lib_dir  
-	 */ 
-	function require_lib_dir( $lib_dir ) { 
-		return JETPACK__PLUGIN_DIR . 'lib'; 
-	} 
+	 * Returns the location of Jetpack's lib directory. This filter is applied
+	 * in require_lib().
+	 *
+	 * @filter require_lib_dir
+	 */
+	function require_lib_dir( $lib_dir ) {
+		return JETPACK__PLUGIN_DIR . 'lib';
+	}
 
 	/**
 	 * Is Jetpack active?
@@ -794,6 +839,47 @@ class Jetpack {
 			}
 		}
 		return apply_filters( 'jetpack_get_default_modules', $return, $min_version, $max_version );
+	}
+
+	/**
+	 * Checks activated plugins during auto-activation to determine
+	 * if any of those plugins are in the list with a corresponding module
+	 * that is not compatible with the plugin. The module will not be allowed
+	 * to auto-activate.
+	 *
+	 * @since 2.6
+	 * @uses jetpack_get_default_modules filter
+	 * @param array $modules
+	 * @return array
+	 */
+	function filter_default_modules( $modules ) {
+		$active_plugins = get_option( 'active_plugins', array() );
+		if ( is_multisite() ) {
+			// Due to legacy code, active_sitewide_plugins stores them in the keys,
+			// whereas active_plugins stores them in the values.
+			$network_plugins = array_keys( get_site_option( 'active_sitewide_plugins', array() ) );
+			if ( $network_plugins ) {
+				$active_plugins = array_merge( $active_plugins, $network_plugins );
+			}
+		}
+		sort( $active_plugins );
+
+		// For each module we'd like to auto-activate...
+		foreach ( $modules as $key => $module ) {
+			// If there are potential conflicts for it...
+			if ( ! empty( $this->conflicting_plugins[ $module ] ) ) {
+				// For each potential conflict...
+				foreach ( $this->conflicting_plugins[ $module ] as $title => $plugin ) {
+					// If that conflicting plugin is active...
+					if ( in_array( $plugin, $active_plugins ) ) {
+						// Remove that item from being auto-activated.
+						unset( $modules[ $key ] );
+					}
+				}
+			}
+		}
+
+		return $modules;
 	}
 
 	/**
@@ -3304,7 +3390,7 @@ p {
 
 		// This should always have gone through Jetpack_Signature::sign_request() first to check $timestamp an $nonce
 		$timestamp = (int) $timestamp;
-		$nonce     = $wpdb->escape( $nonce );
+        $nonce       = esc_sql( $nonce );
 
 		// Raw query so we can avoid races: add_option will also update
 		$show_errors = $wpdb->show_errors( false );
