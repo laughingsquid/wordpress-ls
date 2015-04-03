fb7df819bf80258f805cb493fd2165a609823155
Shelby DeNike <shelby@sd3labs.com>
2015-04-03 17:47:04 -0600

    Nightly Update

--- wp-content/plugins/bruteprotect/admin/mybp-sections/footer.php
+++
@@ -1,8 +0,0 @@
-<div id="brute_footer">
-    <p><a href="?page=bruteprotect-debug">BruteProtect Debug</a></p>
-</div>
-<script>
-    jQuery(document).ready(function () {
-        jQuery(document).foundation();
-    });
-</script>
